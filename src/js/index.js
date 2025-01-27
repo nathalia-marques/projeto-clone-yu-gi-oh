2e712e3b8b8951e16cadda14d198f40a878d3f08
Nathália Marques <144337590+nathalia-marques@users.noreply.github.com>
2025-01-26 22:11:58 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,27 @@ const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+cartoes.forEach(cartao => {
+  cartao.addEventListener("click", function() {
+    const cartaVirada = cartao.querySelector(".carta-virada");
+
+    //virar o cartao
+    cartao.classList.toggle("virar");
+    //mostrar o fundo da carta
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+  })
+});
+
 btnAvancar.addEventListener("click", function() { 
-    if(cartaoAtual === cartoes.length - 1) return;
+  if(cartaoAtual === cartoes.length - 1) return;
 
-    esconderCartaoSelecionado();
+  esconderCartaoSelecionado();
 
-    cartaoAtual++;
-    mostrarCartao(cartaoAtual);
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
 });
 
 btnVoltar.addEventListener("click", function() { 
